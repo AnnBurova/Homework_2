e7bb9c9325f4fd122c1e889cd3d83d50ac698df4
AnnBurova <62078136+AnnBurova@users.noreply.github.com>
2020-03-31 23:06:26 +0300

    Add files via upload

---
+++ HomeWork2.py
@@ -0,0 +1,27 @@
+# Создать функцию frame_codon_count(dna), которая принимает цепочку ДНК в качестве аргумента
+# и возвращает объект (ассоциативный массив / словарь), который должен содержать строки-триплеты(кодоны)
+# в качестве ключей и количество повторений этих подстрок в качестве значений ключей
+# Ассоциативный массив должен содержать только те ключи(кодоны) которые существуют в исходной цепочке ДНЕ,
+# которую иы подаем в качестве аргумента функции. К тому же, кодоном считается последовательность из 3 символов
+# 0,1,2; 3,4,5, 6,7,8 и тд. т.е. элементы кратные трем. Если длинна строки переданной в качестве аргумента не кратна 3
+# последний один или 2 символа игнорируются
+
+
+def frame_codon_count(dna):
+    dna_list = list(dna)
+    dna_leng = len(dna)
+    codons_list = []
+    uniq_codons_list = []
+    codons_dict = {}
+    for index in range(0, dna_leng // 3):
+        codon_index = index*3
+        codon = ''.join(dna_list[codon_index:codon_index+3])
+        codons_list.append(codon)
+        if (not codon in uniq_codons_list):
+            uniq_codons_list.append(codon)
+        for uniq_codon in uniq_codons_list:
+            codons_dict[uniq_codon] = codons_list.count(uniq_codon)
+
+    return codons_dict
+
+print(frame_codon_count('AGGGATTGCTTAAGG'))
